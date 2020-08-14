01053bee74b42e09fe4e9830937dc525a478dcf1
CalidoparaelFrio <69690176+CalidoparaelFrio@users.noreply.github.com>
2020-08-14 11:58:37 -0500

    Add files via upload

---
+++ scriptMenu.js
@@ -0,0 +1,209 @@
+const cambiarlinea1 = () => {
+    let menuHa = document.querySelector('.menuHamb');
+    let changeL = document.querySelector('.change1');
+    let menuOpen = false;
+    menuHa.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeL.classList.add('activeMenu');
+            menuOpen = true;
+        } else {
+            changeL.classList.remove('activeMenu');
+            menuOpen = false;
+        }
+    })
+
+}
+cambiarlinea1()
+
+const cambiarlinea2 = () => {
+    let menuHa = document.querySelector('.menuHamb');
+    let changeL = document.querySelector('.change2');
+    let menuOpen = false;
+    menuHa.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeL.classList.add('activeMenu');
+            menuOpen = true;
+        } else {
+            changeL.classList.remove('activeMenu');
+            menuOpen = false;
+        }
+    })
+
+}
+cambiarlinea2()
+
+const cambiarlinea3 = () => {
+    let menuHa = document.querySelector('.menuHamb');
+    let changeL = document.querySelector('.change3');
+    let menuOpen = false;
+    menuHa.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeL.classList.add('activeMenu');
+            menuOpen = true;
+        } else {
+            changeL.classList.remove('activeMenu');
+            menuOpen = false;
+        }
+    })
+
+}
+cambiarlinea3()
+
+const showtabs = () => {
+    let menuHa = document.querySelector('.menuHamb');
+    let changeL = document.querySelector('.show');
+    let menuOpen = false;
+    menuHa.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeL.classList.add('activeMenu');
+            menuOpen = true;
+        } else {
+            changeL.classList.remove('activeMenu');
+            menuOpen = false;
+        }
+    })
+
+}
+showtabs()
+
+
+const changeRow = () => {
+    let lineScro = document.querySelector('.footScroll');
+    let changeR = document.querySelector('.showRow');
+    let menuOpen = false;
+    lineScro.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeR.classList.add('activeRow');
+            menuOpen = true;
+        } else {
+            changeR.classList.remove('activeRow');
+            menuOpen = false;
+        }
+    })
+
+}
+changeRow()
+
+const changeRow2 = () => {
+    let lineScro = document.querySelector('.footScroll2');
+    let changeR = document.querySelector('.showRow2');
+    let menuOpen = false;
+    lineScro.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeR.classList.add('activeRow2');
+            menuOpen = true;
+        } else {
+            changeR.classList.remove('activeRow2');
+            menuOpen = false;
+        }
+    })
+
+}
+changeRow2()
+
+const changeRow3 = () => {
+    let lineScro = document.querySelector('.footScroll3');
+    let changeR = document.querySelector('.showRow3');
+    let menuOpen = false;
+    lineScro.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeR.classList.add('activeRow3');
+            menuOpen = true;
+        } else {
+            changeR.classList.remove('activeRow3');
+            menuOpen = false;
+        }
+    })
+
+}
+changeRow3()
+
+const showContact = () => {
+    let lineScro = document.querySelector('.footScroll');
+    let changeC = document.querySelector('.showContact');
+    let menuOpen = false;
+    lineScro.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeC.classList.add('activeContact');
+            menuOpen = true;
+        } else {
+            changeC.classList.remove('activeContact');
+            menuOpen = false;
+        }
+    })
+
+}
+showContact()
+
+const showPay = () => {
+    let lineScroP = document.querySelector('.footScroll2');
+    let changeP = document.querySelector('.showPay');
+    let menuOpen = false;
+    lineScroP.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeP.classList.add('activePay');
+            menuOpen = true;
+        } else {
+            changeP.classList.remove('activePay');
+            menuOpen = false;
+        }
+    })
+
+}
+showPay()
+
+const showPay2 = () => {
+    let lineScroP = document.querySelector('.footScroll2');
+    let changeP = document.querySelector('.showPay2');
+    let menuOpen = false;
+    lineScroP.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeP.classList.add('activePay');
+            menuOpen = true;
+        } else {
+            changeP.classList.remove('activePay');
+            menuOpen = false;
+        }
+    })
+
+}
+showPay2()
+
+const showQuestions = () => {
+    let lineScroQ = document.querySelector('.footScroll3');
+    let changeQ = document.querySelector('.showQuestion');
+    let menuOpen = false;
+    lineScroQ.addEventListener('click', () => {
+        console.log('escuchaaaaaa')
+        if (!menuOpen) {
+
+            changeQ.classList.add('activeQuestion');
+            menuOpen = true;
+        } else {
+            changeQ.classList.remove('activeQuestion');
+            menuOpen = false;
+        }
+    })
+
+}
+showQuestions()
